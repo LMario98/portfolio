3a2dc1d9bdca29fbaa6b88f32fb2310af8f728d6
LMario98 <lo-coco-mario-@hotmail.it>
2025-11-14 00:16:06 +0100

    Enhance hero section with dynamic greeting and update persona description for clarity

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     role: 'Full Stack Software Developer',
     email: 'lo-coco-mario-@hotmail.it',
     location: 'Cork, Ireland',
-    description: 'I am a expertise software developer with experience in front-end and back-end technologies. I love creating innovative and functional solutions that improve the user experience.'
+    description: 'I am a software developer with expertise in front-end and back-end technologies. Eager to learn, reliable, and enjoy working in a team.'
   };
 
   document.getElementById('hero-name').textContent = persona.name;
@@ -14,7 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // Impostazione dell'anno corrente nel footer
   //auto update year
   document.getElementById('year').textContent = new Date().getFullYear();
+// aggiungere una funzione che cambia il saluto dinamicamente
 
+function changeWave(){
+  const rotationWave = ["ciao", "Hello", "Hi"];
+  const num = Math.floor(Math.random() * 3)
+  
+
+const foo = document.getElementsByClassName('wave')[0].textContent = rotationWave[num];
+
+
+console.log(foo)
+};
+changeWave()
   // Aggiornamento sezione About
   const aboutSection = document.querySelector('#about p.muted');
   if (aboutSection) aboutSection.textContent = persona.description;
